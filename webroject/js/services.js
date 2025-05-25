942a67bd0ac07eb92a5316314321fb9c518f2e37
Vladislav0-hub <vlad.vasilev.010203@gmail.com>
2025-05-25 23:40:44 +0800

    Add files via upload
    Добавил "Услуги" и "Информацию о сайте"

---
+++ webroject/js/services.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $(".box").click(function () {
+    var text = $(this).data("text");
+    $(".content").text(text);
+  });
+});
+
+$(document).ready(function () {
+  $("#games-amount").on("input", function () {
+    var value = $(this).val();
+    $("#current-value").text(value);
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const themeSelector = document.getElementById('theme-selector');
+  const resetThemeButton = document.getElementById('reset-theme');
+
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+      document.documentElement.setAttribute('data-theme', savedTheme);
+      themeSelector.value = savedTheme;
+  }
+
+  themeSelector.addEventListener('change', (event) => {
+      const selectedTheme = event.target.value;
+      document.documentElement.setAttribute('data-theme', selectedTheme);
+      localStorage.setItem('theme', selectedTheme);
+  });
+
+  resetThemeButton.addEventListener('click', () => {
+      localStorage.removeItem('theme');
+      document.documentElement.removeAttribute('data-theme');
+      themeSelector.value = 'light';
+  });
+});
